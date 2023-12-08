b6d383484e79e33f9598f1057767a1168cd94f4c
ayaka <ayaka.murakami1204@gmail.com>
2023-12-08 20:17:07 +0900

    ensyu5-2

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,8 @@
 public class Test {
 	public static void main(String[]args) {
 		Person taro=new Person();
-		taro.name="saburo";
-		taro.age=0;
+		taro.name="名前なし";
+		taro.age=25;
 		System.out.println(taro.name);
 		System.out.println(taro.age);
 		
